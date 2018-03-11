7cbad815dba1d361ae6f1deffd83cffb172d8611
locphan2207 <locphan2207@gmail.com>
2018-03-11 13:59:11 -0700

    fix bug, get victory work

--- app/component/App/LineGraph.js
+++ app/component/App/LineGraph.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {View, Text
 } from 'react-native';
-// import {VictoryBar, VictoryChart, VictoryTheme} from 'victory-native';
+import {VictoryBar, VictoryChart, VictoryTheme} from 'victory-native';
 
 
 export default class LineGraph extends React.Component {
   render() {
     return (
       <View>
-        <Text>Test</Text>
+        <VictoryChart width={350} theme={VictoryTheme.material}>
+          <VictoryBar data={this.props.data} x="time" y="emotion" />
+        </VictoryChart>
       </View>
     );
   }
